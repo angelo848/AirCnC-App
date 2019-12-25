2e71fff6bbb3d8894a68841fc20fb6746c1c9b03
Angelo Salles <angelo_848@live.com>
2019-12-25 20:49:53 -0300

    finished

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,111 +1,128 @@
 import React, { useState, useEffect } from 'react'
-import { withNavigation } from "react-navigation";
-import { View, StyleSheet, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+import { withNavigation } from 'react-navigation'
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity
+} from 'react-native'
 
 import api from '../services/api'
 
 function SpotList({ tech, navigation }) {
-    const [spots, setSpots] = useState([])
+  const [spots, setSpots] = useState([])
 
-    useEffect(() => {
-        async function loadSpots() {
-            const response = await api.get('/spots', {
-                params: { tech }
-            })
+  useEffect(() => {
+    async function loadSpots() {
+      const response = await api.get('/spots', {
+        params: { tech }
+      })
 
-            setSpots(response.data)
-        }
-
-        loadSpots()
-    }, [])
-
-    function handleNavigate(id) {
-        navigation.navigate('Book', { id })
+      setSpots(response.data)
     }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+    loadSpots()
+  }, [])
 
-            <FlatList
-                style={styles.list}
-                data={spots}
-                keyExtractor={spot => spot._id}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                renderItem={({ item }) => (
-                    <View style={styles.listItem}>
-                        <Image style={styles.thumbnail} source={{ uri: 'http://midiaboom.com.br/wp-content/uploads/2012/02/Google200481.jpg' }} />
-                        <Text style={styles.company}>{item.company}</Text>
-                        <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
-                            <Text style={styles.buttonText}>Solicitar reseva</Text>
-                        </TouchableOpacity>
-                    </View>
-                )}
+  function handleNavigate(id) {
+    navigation.navigate('Book', { id })
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Empresas que usam <Text style={styles.bold}>{tech}</Text>
+      </Text>
+
+      <FlatList
+        style={styles.list}
+        data={spots}
+        keyExtractor={spot => spot._id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Image
+              style={styles.thumbnail}
+              source={{ uri: item.thumbnail_url }}
             />
-        </View>
-    )
+            <Text style={styles.company}>{item.company}</Text>
+            <Text style={styles.price}>
+              {item.price ? `R$${item.price}/dia` : 'GRATUITO'}
+            </Text>
+            <TouchableOpacity
+              onPress={() => handleNavigate(item._id)}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Solicitar reseva</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 30,
-    },
+  container: {
+    marginTop: 30
+  },
 
-    title: {
-        fontSize: 20,
-        color: '#444',
-        paddingHorizontal: 20,
-        marginBottom: 15,
-    },
+  title: {
+    fontSize: 20,
+    color: '#444',
+    paddingHorizontal: 20,
+    marginBottom: 15
+  },
 
-    bold: {
-        fontWeight: 'bold'
-    },
+  bold: {
+    fontWeight: 'bold'
+  },
 
-    list: {
-        paddingHorizontal: 20,
-    },
+  list: {
+    paddingHorizontal: 20
+  },
 
-    listItem: {
-        marginRight: 15,
-    },
+  listItem: {
+    marginRight: 15
+  },
 
-    thumbnail: {
-        width: 200,
-        height: 120,
-        resizeMode: 'cover',
-        borderRadius: 2,
-    },
+  thumbnail: {
+    width: 200,
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 2
+  },
 
-    company: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#333',
-        marginTop: 10,
-    },
+  company: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginTop: 10
+  },
 
-    price: {
-        fontSize: 15,
-        color: '#999',
-        marginTop: 5,
-    },
+  price: {
+    fontSize: 15,
+    color: '#999',
+    marginTop: 5
+  },
 
-    button: {
-        height: 32,
-        backgroundColor: '#f05a5b',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2,
-        marginTop: 15
-    },
+  button: {
+    height: 32,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+    marginTop: 15
+  },
 
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 15
-    }
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 15
+  }
 })
 
 export default withNavigation(SpotList)
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,43 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from "react-native";
+import React, { useState, useEffect } from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Image,
+  AsyncStorage
+} from 'react-native'
 
 import Spotlist from '../components/SpotList'
 
 import logo from '../assets/logo.png'
 
 export default function List() {
-    const [techs, setTechs] = useState([])
+  const [techs, setTechs] = useState([])
 
-    useEffect(() => {
-        AsyncStorage.getItem('techs').then(storagedTechs => {
-            const techsArray = storagedTechs.split(',').map(tech => tech.trim())
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim())
 
-            setTechs(techsArray)
-        })
-    }, [])
+      setTechs(techsArray)
+    })
+  }, [])
 
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Image style={styles.logo} source={logo} />
-
-            <ScrollView>
-                {techs.map(tech => <Spotlist key={tech} tech={tech} />)}
-            </ScrollView>
-        </SafeAreaView>
-    )
-
+      <ScrollView>
+        {techs.map(tech => (
+          <Spotlist key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1
+  },
 
-    logo: {
-        height: 32,
-        resizeMode: "contain",
-        alignSelf: 'center',
-        marginTop: 35,
-    },
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 35
+  }
 })
